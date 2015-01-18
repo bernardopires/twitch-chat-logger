764370505ec4cbe21c54c949940538fab1a6efef
Bernardo Pires <carneiro.be@gmail.com>
2015-01-18 19:53:37 +0100

    Catching more exceptions that can happen when making a request.

--- utils.py
+++ utils.py
@@ -2,12 +2,14 @@ import json
 import requests
 import time
 
+from requests.exceptions import ConnectionError, SSLError
 
 def get_top_streams(n):
     twitch_api_url = 'https://api.twitch.tv/kraken/streams/?limit=%i' % n
     try:
         return json.loads(requests.get(twitch_api_url).text)['streams']
-    except ValueError:
+    except (ValueError, ConnectionError, SSLError):
+        time.sleep(5)
         return get_top_streams(n)
 
 
